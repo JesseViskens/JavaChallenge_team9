a1f8cfe0403959a9d45f52eae57cb96ec490fcdd
PC-EVANNYLEN\elise <elisevannylen@gmail.com>
2017-11-24 13:22:34 +0100

    zaal service

--- angular-src/src/app/components/admin-zalen/admin-zalen.component.ts
+++ angular-src/src/app/components/admin-zalen/admin-zalen.component.ts
@@ -13,10 +13,10 @@ export class AdminZalenComponent implements OnInit {
 
   ngOnInit() {
     this.zalen = [
-      new Zaal(1, "CompUzaal 1", "CompUzaal 1 heeft 20 vaste computers en 5 laptops", 40, "img.png", "0900", "2100", 20),
-      new Zaal(2, "DansZaal", "balletbar en spiegels", 40, "img.png", "0900", "2100", 20),
-      new Zaal(3, "FeestZaal", "tafels en stoelen aanwezig", 40, "img.png", "0900", "2100", 20),
-      new Zaal(4, "SuperZaal", "opdeelbaar in 3", 40, "img.png", "0900", "2100", 20)
+      new Zaal(1, "CompUzaal 1", "CompUzaal 1 heeft 20 vaste computers en 5 laptops", 40, "http://via.placeholder.com/350x150", "0900", "2100", 20),
+      new Zaal(2, "DansZaal", "balletbar en spiegels", 40, "http://via.placeholder.com/350x150", "0900", "2100", 20),
+      new Zaal(3, "FeestZaal", "tafels en stoelen aanwezig", 40, "http://via.placeholder.com/350x150", "0900", "2100", 20),
+      new Zaal(4, "SuperZaal", "opdeelbaar in 3", 40, "http://via.placeholder.com/350x150.png", "0900", "2100", 20)
     ];
   }
 }
