4c7ab6956c73f8c5cc7b253ba036255c93ced296
PC-EVANNYLEN\elise <elisevannylen@gmail.com>
2017-11-24 09:03:53 +0100

    authentication service

--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, EventEmitter } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import Gebruiker from '../models/gebruiker.model';
 import autoBind from 'auto-bind';
 import Config from "../config";
@@ -34,18 +34,20 @@ export class AuthService {
   async getUser(){
     try{
       console.log("Getting user");
-      let result:any= await this.http.get(Config.host + "/auth", HttpHelper.getOptions()).toPromise();
+      let headers = new HttpHeaders().set('content-type', 'application/json').set("Authorization", localStorage.getItem("authKey"));
+      let result:any= await this.http.get(Config.host + "/auth", headers).toPromise();
       this.gebruiker = new Gebruiker(result);
       this.onUserFetched.emit(this.gebruiker);
     }catch(err){
-      HttpHelper.onError(err);
+      console.log(err);
     }
   }
 
   async login(email: string, password: string){
     console.log("Logging in");
     try{
-      let result: any = await this.http.post(Config.host + "/auth/login", {email, password},{headers:HttpHelper.getHeadersNoAuth()}).toPromise();
+      let headers = new HttpHeaders().set('content-type', 'application/json');
+      let result: any = await this.http.post(Config.host + "/auth/login", {email, password},{headers:headers}).toPromise();
       localStorage.setItem("authKey", result.token);
       this.gebruiker = new Gebruiker(result);
       this.onLogin.emit(this.gebruiker);
