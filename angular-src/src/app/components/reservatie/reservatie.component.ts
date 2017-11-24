2b217d9dc3df66bfe214edaf008fd0a069608949
Robin van Utrecht <robinvutrecht@gmail.com>
2017-11-24 10:17:35 +0100

    Bugfix reservatie

--- angular-src/src/app/components/reservatie/reservatie.component.ts
+++ angular-src/src/app/components/reservatie/reservatie.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input, Output, EventEmitter } from "@angular/core";
 import {Reservatie} from "../../models/reservatie.model";
 import {Zaal} from "../../models/zaal.model";
-import {Materiaal} from "../../models/materiaal.model";
 
 @Component({
     selector: 'app-reservatie',
@@ -11,14 +10,8 @@ import {Materiaal} from "../../models/materiaal.model";
 
 export class ReservatieComponent{
   zaal: Zaal;
-  materiaal: Materiaal[] = [
-    new Materiaal(1, "spiegel", 7),
-    new Materiaal(2, "Computer", 20),
-    new Materiaal(3, "Laptop", 10),
-    new Materiaal(4, "valmat", 5)
-  ];
 
   constructor(){
-    this.zaal =  new Zaal(1, "Computerzaal 1", "beschrijving", 40, "img.png", "0900", "2100", 20, this.materiaal);
+    this.zaal =  new Zaal(1, "Computerzaal 1", "beschrijving", 40, "img.png", "0900", "2100", 20);
   }
 }
