962dc0c9c0c8c570adb592ffd6d2a363d568deea
Unknown <roby1996_10@hotmail.com>
2017-11-24 21:26:04 +0100

    admin-zalen opmaak

--- angular-src/src/app/components/admin-zalen/admin-zalen.component.ts
+++ angular-src/src/app/components/admin-zalen/admin-zalen.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Zaal} from "../../models/zaal.model";
 import {ZaalService} from "../../services/zaal.service";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-admin-zalen',
@@ -8,17 +9,15 @@ import {ZaalService} from "../../services/zaal.service";
   styleUrls: ['./admin-zalen.component.css']
 })
 export class AdminZalenComponent implements OnInit {
-  zalen:Zaal[];
+  zalen: Zaal[];
 
-  constructor(private zaalService:ZaalService) { }
+  constructor(private http: HttpClient, private zaalService: ZaalService) {
+  }
 
   ngOnInit() {
-    this.zalen = [
-      new Zaal(1, "CompUzaal 1", "CompUzaal 1 heeft 20 vaste computers en 5 laptops", 40, "http://via.placeholder.com/350x150", "0900", "2100", 20),
-      new Zaal(2, "DansZaal", "balletbar en spiegels", 40, "http://via.placeholder.com/350x150", "0900", "2100", 20),
-      new Zaal(3, "FeestZaal", "tafels en stoelen aanwezig", 40, "http://via.placeholder.com/350x150", "0900", "2100", 20),
-      new Zaal(4, "SuperZaal", "opdeelbaar in 3", 40, "http://via.placeholder.com/350x150.png", "0900", "2100", 20)
-    ];
+    this.zaalService.getZalen().then(zalen=>this.zalen = zalen);
+    console.log(this.zalen);
+
   }
 
   onDelete(id){
