98553dca4f508b69f9703bb91b486667e9fbe5ce
PC-EVANNYLEN\elise <elisevannylen@gmail.com>
2017-11-26 16:37:08 +0100

    deelzaal component added

---
+++ angular-src/src/app/components/admin-deelzaalwijzigen/admin-deelzaalwijzigen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-deelzaalwijzigen',
+  templateUrl: './admin-deelzaalwijzigen.component.html',
+  styleUrls: ['./admin-deelzaalwijzigen.component.css']
+})
+export class AdminDeelzaalwijzigenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
